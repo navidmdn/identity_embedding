ecd62fed2f04d52936dd9f36fe1e4842457b8084
Navid Madani <navidmdn74@gmail.com>
2023-06-21 11:40:14 -0400

    fixed projections to a manual and classic

--- src/inference/projection.py
+++ src/inference/projection.py
@@ -2,92 +2,98 @@ from sentence_transformers import SentenceTransformer
 from numpy import linalg as LA
 import numpy as np
 
-# projection_measures = [
-#
-#     {
-#         "group":
-#             "age",
-#         "names": ['young', 'old'],
-#         "sets": [['young', 'youthful', "15 years old", "18 years old"],
-#                  ['old', 'elderly', 'retired', 'grandparent']],
-#         "paper":
-#             "this_long",
-#         "is_paired":
-#             True
-#     },
-#     {
-#         "group":
-#             "partisanship",
-#         "names": ['democrat', 'republican'],
-#         "sets": [['democrat', 'left-leaning', 'liberal', 'bluewave'],
-#                  ['republican', 'right-leaning', 'conservative', 'maga']],
-#         "paper":
-#             "unk",
-#         "is_paired":
-#             True
-#     },
-#
-#     {
-#         "group":
-#             "establishment",
-#         "names": ['antiestablishment', 'establishment'],
-#         "sets": [['anti-establishment', 'draintheswamp', 'socialist', 'truth seeker'],
-#                  ['proud democrat', 'proud republican', 'uniteblue', 'constitutionalist']],
-#         "paper":
-#             "unk",
-#         "is_paired":
-#             True
-#     },
-#
-#     {
-#         "group":
-#             "religion",
-#         "names": ['atheist', 'religious'],
-#         "sets": [['atheist', 'agnostic', 'non-believing', 'secular', 'not religious'],
-#                  ['religious', 'faithful', 'god', 'believe in lord',
-#                   'jesus', 'christian']],
-#         "paper":
-#             "unk",
-#         "is_paired":
-#             True
-#     },
-#     {
-#         "group":
-#             "gender",
-#         "names": ['woman', 'man'],
-#         "sets": [[
-#             'woman', 'girl', 'she',
-#             'mother',
-#             'female', 'her',
-#             'herself', 'dad'
-#         ],
-#             [
-#                 'man', 'boy', 'he', 'father', 'male', 'him',
-#                 'himself', 'mom'
-#             ]],
-#         "paper":
-#             "bolukbasi_words",
-#         "is_paired":
-#             True
-#     },
-#     {
-#         "group":
-#             "politics",
-#         "names": ['apolotical', 'political'],
-#         "sets": [[
-#             'music', 'sports', 'culture', 'tech'
-#         ],
-#             [
-#                 'politics', 'political', 'democrat', 'republican'
-#             ]],
-#         "paper":
-#             "bolukbasi_words",
-#         "is_paired":
-#             True
-#     }
-# ]
 
-projection_measures = [
+new_projection_measures = [
+
+    {
+        "group":
+            "age",
+        "names": ['young', 'old'],
+        "sets": [['15 years old', '18 years old', 'sophomore in college', 'student at', "umich'22", '18', '21'],
+                 ['retired person', 'I’m old', '50 years old', '65 years old', '61yr old', 'grandparent of',
+                  'old man', 'old woman', 'grandma to', 'grandpa to', 'tenured', 'long career']],
+        "paper":
+            "this_long",
+        "is_paired":
+            True
+    },
+    {
+        "group":
+            "partisanship",
+        "names": ['democrat', 'republican'],
+        "sets": [['pro socialism', 'liberal democrat', 'never trump', 'proud democrat', 'vote blue no matter who',
+                  '#resist', '#voteblue', '#nevertrump', 'left leaning', '#democraticdownballot',
+                    '#notvotebluenomatterwho', '#bidenharris', '#resist', '#bluewave', '#democraticsocialist'],
+                 ['right leaning', 'trump won', 'never biden', 'fuck biden', '#maga', '#kag', 'Trump conservative',
+                  'conservative and America First', 'proud Trump supporter', 'trump fan', '#MAGA Republican',
+                  'constitutional conservative patriot', '#trump2024']],
+        "paper":
+            "unk",
+        "is_paired":
+            True
+    },
+
+    {
+        "group":
+            "religion",
+        "names": ['atheist', 'religious'],
+        "sets": [['atheist', 'nonbeliever', 'proud atheist', 'totally secular', '#cancelreligion'],
+                 ['Catholic', 'jesus christ', 'follower of christ', 'priest', 'lover of jesus', 'christian episcopalian',
+                  'jesus loving christian', 'john 3:16', 'gospel of the lord jesus christ', 'minister at united church',
+                  'christ-follower', 'god first', 'isaiah 55:6', 'woman of faith', 'man of faith']],
+        "paper":
+            "unk",
+        "is_paired":
+            True
+    },
+    {
+        "group":
+            "gender",
+        "names": ['woman', 'man'],
+        "sets": [[
+            'sister', 'wife', 'mother', 'Proud Mama and Wife', 'grandmother of', 'mother of one', 'mama of one',
+            'wife of', 'Loving Wife', 'she', 'her', 'hers'
+        ],
+            [
+                'husband to', 'brother', 'husband', 'father', 'grandfather of one', 'father of one', 'Loving husband',
+                'he', 'him', 'his', 'son', 'brother', 'brother-in-law', 'uncle', 'nephew'
+            ]],
+        "paper":
+            "bolukbasi_words",
+        "is_paired":
+            True
+    },
+    {
+        "group":
+            "politics",
+        "names": ['apolotical', 'political'],
+        "sets": [
+            [
+                'football fan', 'soccer fan', 'hockey fan', 'all about sports', 'all about music', 'art and culture',
+                'looking for love', 'marketing', 'bitcoin cryptocurrency', 'music lover', 'alternative rock',
+                'science writer', 'poetry', 'anime, cartoons', 'nascar', 'full-stack javascript software engineer',
+                'musician, artist', 'football coach', 'movie lover', 'music producer', 'digital marketer, marketing strategist',
+                'market analyst, trader, equities', 'celebrity and entertainment news', 'telling stories, making films',
+                'blockchain', 'cooking, books', 'theatre, opera, music, art', 'software developer', 'bitcoin',
+                'seo specialist', 'film maker'
+            ],
+            [
+                'pro socialism', 'liberal democrat', 'never trump', 'proud democrat', 'vote blue no matter who',
+                  '#resist', '#voteblue', '#nevertrump', 'left leaning', '#democraticdownballot',
+                    '#notvotebluenomatterwho', '#bidenharris', '#resist', '#bluewave', '#democraticsocialist',
+                 'right leaning', 'trump won', 'never biden', 'fuck biden', '#maga', '#kag', 'Trump conservative',
+                  'conservative and America First', 'proud Trump supporter', 'trump fan', '#MAGA Republican',
+                  'constitutional conservative patriot', '#trump2024'
+
+            ]],
+        "paper":
+            "bolukbasi_words",
+        "is_paired":
+            True
+    }
+]
+
+old_projection_measures = [
 
     {
         "group":
@@ -102,7 +108,7 @@ projection_measures = [
     },
     {
         "group":
-        "politics",
+        "partisanship",
         "names": ['democrat', 'republican'],
         "sets": [['democratic party supporter', 'left-leaning', 'democrat'], ['republican party supporter', 'right-leaning', 'republican']],
         "paper":
@@ -122,43 +128,30 @@ projection_measures = [
     },
     {
         "group":
-        "education",
-        "names": ['educated', 'uneducated'],
-        "sets": [['educated', 'higher education'], ['uneducated', 'unschooled']],
-        "paper":
-        "unk",
-        "is_paired":
-        True
-    },
-    {
-        "group":
-        "employment status",
-        "names": ['employed', 'unemployed'],
-        "sets": [['employed', 'hired', 'working', 'on the job'], ['unemployed', 'jobless', 'out of work', 'retired']],
-        "paper":
-        "unk",
-        "is_paired":
-        True
-    },
-    {
-        "group":
         "gender",
         "names": ['woman', 'man'],
         "sets": [
-        #     [
-        #     'woman', 'girl', 'she', 'mother', 'daughter', 'gal', 'female', 'her',
-        #     'herself',
-        # ],
-        # [
-        #  'man', 'boy', 'he', 'father', 'son', 'guy', 'male', 'his',
-        #  'himself'
-        # ]
             ['mother of x', 'grand mother'], ['father of x', 'grand father']
         ],
         "paper":
         "bolukbasi_words",
         "is_paired":
         True
+    },
+    {
+        "group":
+            "politics",
+        "names": ['apolotical', 'political'],
+        "sets": [[
+            'music', 'sports', 'culture', 'tech'
+        ],
+            [
+                'politics', 'political', 'democrat', 'republican'
+            ]],
+        "paper":
+            "bolukbasi_words",
+        "is_paired":
+            True
     }
 ]
 
@@ -229,12 +222,13 @@ def project_embeddings(model, dims, sentence_embeddings):
 
 
 def get_sentence_projections(sentences, model, batch_size=128, device='cpu', show_progress_bar=True, as_dict=False,
-                             measures=None):
+                             measures=None, projection_measures=old_projection_measures):
     print("Getting sentence embeddings")
     sentence_embeddings = model.encode(
         sentences, batch_size=batch_size, show_progress_bar=show_progress_bar, device=device)
 
     dim_dict = load_dimensions_dict(projection_measures)
+    print(dim_dict)
     if measures is not None:
         for k, v in measures.items():
             dim_dict[k] = v
